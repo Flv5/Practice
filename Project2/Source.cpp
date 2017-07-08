a99ffe20ea91421eb347a6d09f83d0f7f482f274
Flavio <0423303@AI911LAB169P12>
2017-07-08 15:29:36 -0700

    TESTING

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -3,9 +3,14 @@ int main()
 
 {
 	//printing!
-	// use std to before to 
-	std::cout << "Hello!\n";
-
+	// use std to before the cout
+	std::cout << "Hello!\n"; // we use cout to print
+	int value;
+	std::cin >> value,"\n";// we use cin to scan
+	std::cout << value, "\n";
+	std::cout << "whats up yo";
 	system("pause");
 	return 0;
+	
+
 }
